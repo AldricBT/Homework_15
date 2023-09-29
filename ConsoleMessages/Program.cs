8d3d6e40c28348224a3ae67fb6e9ec98852d2740
Alexander Kamenskikh <sanyakamen96@mail.ru>
2023-09-29 16:15:38 +0500

    асинхронный вывод потоков

--- ConsoleMessages/Program.cs
+++ ConsoleMessages/Program.cs
@@ -2,8 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
+        {            
+            PrintThreadToConsoleAsync(1000);
+            PrintThreadToConsoleAsync(2000);
+            PrintThreadToConsoleAsync(3000);
+            await PrintLineAsync(1001);
+        }
+
+        async static Task PrintLineAsync(int sleepTime)
         {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine();
+                    Thread.Sleep(sleepTime);                    
+                }
+            });
+
+        }
+        async static Task PrintThreadToConsoleAsync(int sleepTime)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(sleepTime);
+                    Console.WriteLine($"Это поток {Task.CurrentId}, выполняемый с задержкой {sleepTime}");                    
+                }
+            });
             
         }
     }
